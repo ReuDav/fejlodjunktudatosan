cad6e5f1685818bdbdeb944e7cb193502e8365e6
Reucov David <reucovd@gmail.com>
2024-12-17 21:53:38 +0100

    optimized loading performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,50 @@
 "use client"
 
-import CallToAction from "@/../components/call-to-action";
-import SkillsSlider from "../../components/skills-slider";
-import ProjectCard from "../../components/project-cards";
-import ProcessCarousel from "../../components/process-carousel";
-import SkillList from "../../components/skills-list";
-import ExperienceList from "../../components/experience-list";
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+import CallToAction from "@/../components/call-to-action"
+
+const SkillsSlider = dynamic(() => import("../../components/skills-slider"), {
+  loading: () => <div>...</div>
+})
+const ProjectCard = dynamic(() => import("../../components/project-cards"))
+const ProcessCarousel = dynamic(() => import("../../components/process-carousel"))
+const SkillList = dynamic(() => import("../../components/skills-list"))
+const ExperienceList = dynamic(() => import("../../components/experience-list"))
 
 export default function Home() {
     return (
         <main className="min-h-screen">
             <CallToAction/>
-            <SkillsSlider/>
-            <ExperienceList />
-            <ProjectCard/>
-            <ProcessCarousel/>
-            <SkillList skills={["Frontend fejlesztés", "Backend fejlesztés", "UI dizájn", "UX tervezés", "Tesztelés", "Mentorálás", "Tanítás", "Ügyfél-orientált kommunikáció"]} />
+            
+            <Suspense fallback={<div>...</div>}>
+                <SkillsSlider/>
+            </Suspense>
+            
+            <Suspense fallback={<div>...</div>}>
+                <ExperienceList />
+            </Suspense>
+            
+            <Suspense fallback={<div>...</div>}>
+                <ProjectCard/>
+            </Suspense>
+            
+            <Suspense fallback={<div>...</div>}>
+                <ProcessCarousel/>
+            </Suspense>
+            
+            <Suspense fallback={<div>...</div>}>
+                <SkillList skills={[
+                    "Frontend fejlesztés",
+                    "Backend fejlesztés",
+                    "UI dizájn",
+                    "UX tervezés",
+                    "Tesztelés",
+                    "Mentorálás",
+                    "Tanítás",
+                    "Ügyfél-orientált kommunikáció"
+                ]} />
+            </Suspense>
         </main>
-    );
+    )
 }
